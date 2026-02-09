85fc4e09c6b193bf43ff51f808a839ef6999813b
Kareem-Tarek <kareemtarekpk@gmail.com>
2026-02-09 16:31:44 +0200

    some updates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@
 
     <!-- Bootstrap 5 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    @stack('styles')
 </head>
 <body>
     @include('layouts.navigation')
@@ -18,5 +19,6 @@
 
     <!-- Bootstrap 5 JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    @stack('scripts')
 </body>
 </html>
